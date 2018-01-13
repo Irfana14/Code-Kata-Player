31e824deeb0cad81d4cf6a71ca19b8da623e8912
codegokul <35158954+codegokul@users.noreply.github.com>
2018-01-13 12:32:52 +0530

    Add files via upload

---
+++ Isomorphic.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.Arrays;
+import java.util.HashMap;
+public class Isomorphic
+{
+    static int size = 256; 
+    
+    static boolean areIsomorphic(String str1, String str2)
+    {
+        int m = str1.length();
+        int n = str2.length(); 
+        Map<Character, Character> hashTable = new HashMap<Character, Character>();
+        Boolean[] charExists = new Boolean[256];
+        Arrays.fill(charExists, false);
+        if(m != n)
+            return false;
+        for(int i=0;i<m;i++)
+        {
+        	if(charExists[str1.charAt(i)]==false)
+        	{
+        		hashTable.put(str1.charAt(i), str2.charAt(i));
+        	    charExists[str1.charAt(i)]=true;
+        	}
+        	
+        	else if(hashTable.get(str1.charAt(i))!=str2.charAt(i))
+        		return false;        	
+        }
+        return true; 
+    }
+    public static void main (String[] args) 
+    {
+        boolean res = areIsomorphic("aab", "xxy");         
+        System.out.println(res);
+     
+
+    }
+}
